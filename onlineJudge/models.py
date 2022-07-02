f77cafdd5240c25f9c0239f965173b11f56c7503
Swatantra <swatantracool05@gmail.com>
2022-07-02 13:18:03 +0530

    models added

--- onlineJudge/models.py
+++ onlineJudge/models.py
@@ -1,3 +1,41 @@
+from sqlite3 import Timestamp
 from django.db import models
 
-# Create your models here.
+
+class Problems(models.Model):
+    Problem_Name = models.CharField(max_length=50)
+    Problem_Description = models.TextField()
+    difficulty_choice=(
+        ('easy','easy'),
+        ('medium','medium'),
+        ('difficult','difficult'),
+    )
+    Problem_Difficulty = models.CharField(max_length=20,choices=difficulty_choice,default='easy')
+
+    def __str__(self):
+        return self.Problem_Name
+
+class Test_Case(models.Model):
+    PID = models.ForeignKey(Problems, on_delete=models.CASCADE)
+    test_input = models.FileField(upload_to='test_input/')
+    test_output = models.FileField(upload_to='test_output/')
+
+class Solutions(models.Model):
+    PID = models.ForeignKey(Problems,on_delete=models.CASCADE)
+    language_choices=(
+        ('c++','cpp')
+        ('c','c')
+        ('java','java')
+        ('python','python')
+    )
+    Language = models.CharField(max_length=20,choices=language_choices)
+    Code_file = models.FileField(upload_to='Code_file')
+    verdict_choice=(
+        ('Exe','Executing'),
+        ('WA','Wrong Answer'),
+        ('AC','All Correct'),
+        ('TLE','Time Limit Exeeded'),
+        ('MLE','Memory Limit Exeeded'),
+    )
+    verdict = models.CharField(max_length=10,choices=verdict_choice)
+    timestamp= models.TimeField(auto_now_add=True)
